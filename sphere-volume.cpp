d5338c97072e5f975d1446fce7563bcf7ab47bfc
Nazmus Sadat <151758467+Nazmus-Sadat-21@users.noreply.github.com>
2024-10-31 02:15:15 +0600

    Create sphere-volume.cpp

---
+++ sphere-volume.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+using namespace std;
+int main()
+{
+
+  double r , vol;
+  cout<< "Enter the radius of sphere = ";
+  cin>>r;
+
+  vol= (4.0/3)*3.1416*(r*r*r);
+  cout<< "The volume of sphere is = "<< vol;
+
+
+    return 0;
+}
