a214368df4e4d858c68c62748b72507d68171169
Nazmus Sadat <151758467+Nazmus-Sadat-21@users.noreply.github.com>
2024-11-05 22:16:07 +0600

    Create Basic-structure.cpp

---
+++ Basic-structure.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+struct student
+{
+    // string name;
+    // double cg;
+    // string dept;
+
+    void display(string name,double cg,string dept){
+    cout<<"Name = "<<name<<endl;
+    cout<<"CGPA = "<<cg<<endl;
+    cout<<"Department = "<<dept<<endl;
+}
+
+};
+void display(string name,double cg,string dept){
+    cout<<"Name = "<<name<<endl;
+    cout<<"CGPA = "<<cg<<endl;
+    cout<<"Department = "<<dept<<endl;
+}
+
+int main(){
+     student s1,s2;
+     s1.display("Nazmus Sadat",3.51,"CSE");
+     s2.display("Jabir Ahmed",3.42,"CSE");
+}
