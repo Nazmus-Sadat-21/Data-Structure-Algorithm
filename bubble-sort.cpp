7828f7f112d2166706959c578b121debfa2bfff0
Nazmus Sadat <151758467+Nazmus-Sadat-21@users.noreply.github.com>
2024-11-13 20:37:54 +0600

    Create bubble-sort.cpp

---
+++ bubble-sort.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+int main (){
+    int n;
+    cout<<"Enter array size = ";
+    cin>>n;
+    int arr[n];
+    bool sawping;
+
+    for(int i =0;i<n;i++){
+        cin>>arr[i];
+    }
+
+    for(int k =0;k<n-1;k++){
+        sawping = false;
+        for(int j =0;j<n-k-1;j++){
+            if(arr[j]>arr[j+1]){
+                swap(arr[j],arr[j+1]);
+                sawping=true;
+
+            }
+        }
+       
+    }
+    
+    if(!sawping){
+            cout<<"Elements already sorted";
+    }
+    else{
+    cout<<"After sorting array : "<<endl;
+    for(int i =0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+    }
+
+}
