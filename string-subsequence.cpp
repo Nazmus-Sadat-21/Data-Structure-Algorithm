4caf097e544d3dc64eb77d8893fb71f36592f879
Nazmus Sadat <151758467+Nazmus-Sadat-21@users.noreply.github.com>
2024-12-06 23:52:04 +0600

    Update string-subsequence.cpp

--- string-subsequence.cpp
+++ string-subsequence.cpp
@@ -2,26 +2,15 @@
 using namespace std;
 int main(){
 
-    string s = "abchkjhk";
+    string s = "abc";
     string t = "aecdbf";
-    int count =0, n=0,m=0;
+    int count =0;
 
-    if(s.length()>t.length()){
-        cout<<"s is a main string";
-        n= s.length();
-        m=t.length();
-    }
-    else if(t.length()>s.length()){
-        cout<<"t is a main string";
-        n=t.length();
-        m=s.length();
+   
 
-    }
-    cout<<endl;
-
-    for(int i =0;i<n;i++){
-        for(int j = 0;j<m;j++){
-            if(s[i]==t[j]){
+    for(int i =0;i<t.length;i++){
+        for(int j = 0;j<s.length;j++){
+            if(t[i]==s[j]){
                count ++;
             }
             
@@ -30,9 +19,6 @@ int main(){
     if(count==s.length()){
         cout<<"s is a subsequence of t"<<endl;
     }
-    else if(count==t.length()){
-        cout<<"t is a subsequence of s";
-    }
-
+ 
 
 }
