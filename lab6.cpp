89e428b4b0cff3e8e3788c8a41f2a4ebc6c515ad
Nazmus Sadat <151758467+Nazmus-Sadat-21@users.noreply.github.com>
2024-12-16 20:55:20 +0600

    Create lab6.cpp

---
+++ lab6.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int arr[]={12, 9, 4, 99, 120, 1, 3, 10, 23, 45};
+    int size  =sizeof(arr)/sizeof(arr[0]);
+    int count=0,count2=0;
+    cout<<"The sorted array : "<<endl;
+
+    for(int i=0;i<size-1;i++){   
+        count++; 
+        for(int j =i+1;j<size;j++){
+            count2++;
+            if(arr[j]<arr[i]){
+                int temp = arr[j];
+                arr[j]=arr[i];
+                arr[i]=temp;
+                
+            }
+        }
+    }
+
+    for(int i=0;i<size;i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+    cout<<"Number of exchanges = "<<count<<endl;
+    cout<<"number of comparison = "<<count2;
+}
+
